1addacbccb02dbacc79550331804348c72205bd3
ahmadatef17 <144893367+ahmadatef17@users.noreply.github.com>
2023-09-24 17:13:08 +0200

    Update Domain Users Logs on Computers 'with time limit'.ps1

--- Domain Users Logs on Computers 'with time limit'.ps1
+++ Domain Users Logs on Computers 'with time limit'.ps1
@@ -2,19 +2,20 @@
 <# God Willing, this will be used in GPMC, (Find Active, Apply program) #>
 
 <# Variables Declaration #>
-$current_month = [datetime]::now.tostring("yyyy-MM)                     <# Variable Contain Current date 'Only' year-Month #>
+$curr_month = [datetime]::now.tostring("yyyy-MM")                       <# Variable Contain Current date 'Only' year-Month #>
 $koko 		= $Env:ComputerName				        <# Variable Contain Computer Name #>
+$yoyo 		= $Env:UserName                                         <# Variable Contain User Name #>
 $srv_Directory 	= "192.168.24.157"				        <# Variable Contain server address, Where you put your logs files #>
-$logfile 	= "\\$srv_Directory\D$\logs\PC_logs-$current_month.txt"	<# Variable Contain Log file path #>
-$flagfile 	= "\\$srv_Directory\D$\logs\Devices-$current_month.txt"	<# Variable Contain flags file path #>
-$no_of_logons 	= 10						        <# number of logon and logoff you want to get collect, put whatever number you want #>
+$logfile 	= "\\$srv_Directory\D$\logs\Users_logs_$curr_month.txt"	<# Variable Contain Log file path #>
+$flagfile 	= "\\$srv_Directory\D$\logs\Users_$curr_month.txt"	<# Variable Contain flags file path #>
+$no_of_logons 	= 1						        <# number of logon and logoff you want to get collect, put whatever number you want #>
 
 <# if flags file does not exsist, create it #>
 if(!(Test-Path $flagfile))
 { New-Item -ItemType "file" -Path $flagfile }
 
 [bool] $compFlagVal = 0						<# Boolean (True,False) Variable to tell that this computer is already on our log file #>
-$compFlagStr 	= Select-String -Path $flagfile -pattern $koko	<# Variable to search in flags file for the word and print it if exist #>
+$compFlagStr 	= Select-String -Path $flagfile -pattern $yoyo	<# Variable to search in flags file for the word and print it if exist #>
 
 <# If My flag file Contains that ComputerName to print "True" ~ 1 or "false" ~ 0 #>
 if($NULL -eq $compFlagStr){ 
@@ -26,4 +27,4 @@ else{$compFlagVal=1}
 if(!($compFlagVal)){
 Get-WinEvent -Computer "$koko" -FilterHash @{LogName='system';providername='Microsoft-Windows-Winlogon'} -MaxEvents $no_of_logons |
 Select-Object @{n="User";e={(New-Object System.Security.Principal.SecurityIdentifier $_.Properties.Value[1]).Translate([System.Security.Principal.NTAccount])}},@{n="Action";e={if($_.ID -eq 7001) {"Logon"} else {"Logoff"}}},@{n="Time";e={$_.timecreated}},@{n="Computer";e={$koko}},@{n="TimeCommandRun";e={(Get-Date).ToString()}} | Format-Table -Wrap -AutoSize >> $logfile
-$koko >> $flagfile }
+$yoyo >> $flagfile }
